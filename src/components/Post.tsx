d24c0ec507cd64478da30974a4a9b97b1558b44e
b.chirani <chirani124@gmail.com>
2025-11-21 01:02:18 +0100

    updated design for Post.tsx

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -17,19 +17,21 @@ const Post = ({ id, title, body, likeCount, likedByUser, slug }: IPost) => {
 	const toggleLike = () => (likedByUser ? unlikePost(id) : likePost(id));
 
 	return (
-		<div className="mb-0 p-6 bg-white border-b border-b-zinc-200">
-			<Link to="/post/$postId" params={{ postId: slug }} className="hover:bg-gray-50">
-				<h2 className="text-2xl font-semibold">{title}</h2>
-				<p className="text-lg">{body}</p>
+		<div className="mb-0 bg-white border-b border-b-zinc-200">
+			<Link to="/post/$postId" params={{ postId: slug }}>
+				<div className="p-6 hover:opacity-50">
+					<h2 className="text-2xl font-semibold">{title}</h2>
+					<p className="text-lg">{body}</p>
+				</div>
 			</Link>
-			<div className="flex flex-row mt-4">
+			<div className="flex flex-row px-6 py-3">
 				<button
 					type="button"
 					className={`btn btn-ghost rounded-full ${likedByUser ? "text-teal-500" : "text-zinc-600"}`}
 					onClick={toggleLike}
 				>
 					{likeCount}
-					<ThumbsUp className="text-md" fill="--red-500" />
+					<ThumbsUp className="text-md" />
 				</button>
 			</div>
 		</div>
