126316aedf4ef4a756fadef2d1126c82645f053e
b.chirani <chirani124@gmail.com>
2025-11-26 12:55:29 +0100

    updated comment schena

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -36,15 +36,16 @@ export const likes = pgTable(
 	(table) => [primaryKey({ columns: [table.postId, table.userId] })],
 );
 
-export const comments = pgTable(
-	"comments",
-	{
-		userId: text("user_id").references(() => user.id),
-		postId: text("post_id").references(() => posts.id),
-		comment: text("comment").notNull(),
-		createdAt: timestamp("created_at").defaultNow(),
-		updatedAt: timestamp("updated_at"),
-		deletedAt: timestamp("deleted_at"),
-	},
-	(table) => [primaryKey({ columns: [table.postId, table.userId] })],
-);
+export const comments = pgTable("comments", {
+	id: text("id").default(sql`gen_random_uuid()`).primaryKey(),
+	userId: text("user_id")
+		.references(() => user.id)
+		.notNull(),
+	postId: text("post_id")
+		.references(() => posts.id)
+		.notNull(),
+	comment: text("comment").notNull(),
+	createdAt: timestamp("created_at").defaultNow(),
+	updatedAt: timestamp("updated_at"),
+	deletedAt: timestamp("deleted_at"),
+});
