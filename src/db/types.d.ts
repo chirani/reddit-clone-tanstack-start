84e3ce964af4a563603cfca759ef1d050e2400cd
b.chirani <chirani124@gmail.com>
2025-12-11 21:10:48 +0100

    added dummy data "init_data"

--- src/db/types.d.ts
+++ src/db/types.d.ts
@@ -1,3 +1,5 @@
-import type { posts } from "./schema";
+import type { account, posts, user } from "./schema";
 
 export type postInsertType = typeof posts.$inferInsert;
+export type userInsertType = typeof user.$inferInsert;
+export type accountInsertType = typeof account.$inferInsert;
