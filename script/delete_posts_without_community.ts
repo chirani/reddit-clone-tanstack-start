f5fcf21d3ffb10a8a7298b08d4bfb21e49ec7b17
b.chirani <chirani124@gmail.com>
2025-12-15 03:38:07 +0100

    updated functions to arrow function

--- script/delete_posts_without_community.ts
+++ script/delete_posts_without_community.ts
@@ -1,17 +1,15 @@
+import { eq, isNull } from "drizzle-orm";
 import { db } from "../src/db/index.ts";
 import { comments, posts } from "../src/db/schema.ts";
-import {  eq, isNull } from "drizzle-orm";
 
+(async () => {
+	const res = await db.select({ id: posts.id }).from(posts).where(isNull(posts.communityId));
 
+	res.forEach(async (post) => {
+		console.log("Post Id", post.id);
+		await db.delete(comments).where(eq(comments.postId, post.id));
+	});
 
-(async function() {
-    const res =   await db.select({id:posts.id}).from(posts).where(isNull(posts.communityId))
-
-    res.forEach(async (post)=>{
-        console.log("Post Id", post.id)
-        await db.delete(comments).where(eq(comments.postId,post.id))
-    })
-    
-    const res2 =  await db.delete(posts).where(isNull(posts.communityId)).returning()
-    console.log(res2)
+	const res2 = await db.delete(posts).where(isNull(posts.communityId)).returning();
+	console.log(res2);
 })();
--- script/remove_posts_without_user.ts
+++ script/remove_posts_without_user.ts
@@ -1,12 +1,10 @@
+import { count, isNull } from "drizzle-orm";
 import { db } from "../src/db/index.ts";
 import { posts } from "../src/db/schema.ts";
-import { count, isNull } from "drizzle-orm";
 
-
-
-(async function() {
-    const res =  await db.select({count:count()}).from(posts).where(isNull(posts.userId))
-    console.log(res.length)
-    const res2 =  await db.delete(posts).where(isNull(posts.userId)).returning()
-    console.log(res2)
+(async () => {
+	const res = await db.select({ count: count() }).from(posts).where(isNull(posts.userId));
+	console.log(res.length);
+	const res2 = await db.delete(posts).where(isNull(posts.userId)).returning();
+	console.log(res2);
 })();
