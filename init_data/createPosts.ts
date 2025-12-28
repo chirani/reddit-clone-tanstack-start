96236d84bba40482551b8d5bcc094cf57839947d
b.chirani <chirani124@gmail.com>
2025-12-28 15:20:20 +0100

    Adding posts to seed data

--- init_data/createPosts.ts
+++ init_data/createPosts.ts
@@ -1,7 +1,7 @@
 import * as fs from "node:fs/promises";
 import { isNull } from "drizzle-orm";
 import { db } from "../src/db/index.ts";
-import { communities, generateSlug, posts, user } from "../src/db/schema.ts";
+import { communities, communityMemberships, generateSlug, posts, user } from "../src/db/schema.ts";
 
 export type Post = {
 	title: string;
@@ -34,12 +34,17 @@ export const postFormatted: postType[] = typedPostList.map((p) => ({
 	const postsList: postType[] = postFormatted.map((pf) => {
 		const randomUser = Math.floor(Math.random() * userNumber);
 		const randomCommunity = Math.floor(Math.random() * communityNumber);
+
 		return {
 			...pf,
 			userId: createdUsers[randomUser].id,
 			communityId: createCommunities[randomCommunity].id,
 		};
 	});
-
+	const communityMemberList = postsList.map((cm) => ({
+		userId: cm.userId ?? "",
+		communityId: cm.communityId ?? "",
+	}));
+	await db.insert(communityMemberships).values(communityMemberList);
 	await db.insert(posts).values(postsList).returning();
 })();
